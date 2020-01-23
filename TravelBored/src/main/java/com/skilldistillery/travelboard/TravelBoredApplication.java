3c742a1e7b234031350f3f3983a0adc5353cfe0d
Erin Eustice <erinrenee.va@gmail.com>
2020-01-23 11:34:10 -0700

    Stub Boot/MVC Project

---
+++ TravelBored/src/main/java/com/skilldistillery/travelboard/TravelBoredApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.travelboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBoredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBoredApplication.class, args);
+	}
+
+}
