90ec9dd69c827577dca7bf61be1c1dd0a9f8750f
Travis Roberts <travis.c.roberts35@gmail.com>
2020-01-23 17:21:35 -0700

    completed database schema

--- DB/travelbored.sql
+++ DB/travelbored.sql
@@ -16,6 +16,22 @@ CREATE SCHEMA IF NOT EXISTS `travelboreddb` DEFAULT CHARACTER SET utf8 ;
 USE `travelboreddb` ;
 
 -- -----------------------------------------------------
+-- Table `location`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `location` ;
+
+CREATE TABLE IF NOT EXISTS `location` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `country` VARCHAR(100) NULL,
+  `state` VARCHAR(45) NULL,
+  `city` VARCHAR(45) NULL,
+  `address` VARCHAR(200) NULL,
+  `active` TINYINT NULL DEFAULT 1,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `user`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `user` ;
@@ -26,8 +42,213 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` VARCHAR(45) NOT NULL,
   `active` TINYINT NULL DEFAULT 1,
   `role` VARCHAR(45) NULL,
+  `first_name` VARCHAR(45) NULL,
+  `last_name` VARCHAR(45) NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `location_id` INT NOT NULL,
+  `create_date` DATETIME NULL,
+  `user_img_url` VARCHAR(20000) NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC))
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC),
+  INDEX `fk_user_location1_idx` (`location_id` ASC),
+  CONSTRAINT `fk_user_location1`
+    FOREIGN KEY (`location_id`)
+    REFERENCES `location` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `event`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `event` ;
+
+CREATE TABLE IF NOT EXISTS `event` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(50) NULL,
+  `hook` VARCHAR(200) NULL,
+  `description` TEXT NULL,
+  `event_time` TIME NULL,
+  `event_date` DATE NULL,
+  `img_url` VARCHAR(10000) NULL,
+  `event_url` VARCHAR(10000) NULL,
+  `active` TINYINT NULL DEFAULT 1,
+  `location_id` INT NOT NULL,
+  `create_date` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_event_location1_idx` (`location_id` ASC),
+  CONSTRAINT `fk_event_location1`
+    FOREIGN KEY (`location_id`)
+    REFERENCES `location` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `event_details`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `event_details` ;
+
+CREATE TABLE IF NOT EXISTS `event_details` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `detail_name` VARCHAR(45) NULL,
+  `detail_type` VARCHAR(45) NULL,
+  `active` TINYINT NULL DEFAULT 1,
+  `event_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_event_details_event1_idx` (`event_id` ASC),
+  CONSTRAINT `fk_event_details_event1`
+    FOREIGN KEY (`event_id`)
+    REFERENCES `event` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `event_comment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `event_comment` ;
+
+CREATE TABLE IF NOT EXISTS `event_comment` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `content` TEXT NULL,
+  `create_date` DATETIME NULL,
+  `user_id` INT NOT NULL,
+  `event_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_event_comment_user1_idx` (`user_id` ASC),
+  INDEX `fk_event_comment_event1_idx` (`event_id` ASC),
+  CONSTRAINT `fk_event_comment_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_event_comment_event1`
+    FOREIGN KEY (`event_id`)
+    REFERENCES `event` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `group`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `group` ;
+
+CREATE TABLE IF NOT EXISTS `group` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(100) NULL,
+  `hook` VARCHAR(200) NULL,
+  `user_id` INT NOT NULL,
+  `create_date` DATETIME NULL,
+  `active` TINYINT NULL DEFAULT 1,
+  PRIMARY KEY (`id`),
+  INDEX `fk_group_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_group_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `group_comment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `group_comment` ;
+
+CREATE TABLE IF NOT EXISTS `group_comment` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `content` TEXT NULL,
+  `create_date` DATETIME NULL,
+  `group_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_group_comment_group1_idx` (`group_id` ASC),
+  INDEX `fk_group_comment_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_group_comment_group1`
+    FOREIGN KEY (`group_id`)
+    REFERENCES `group` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_group_comment_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user_event`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user_event` ;
+
+CREATE TABLE IF NOT EXISTS `user_event` (
+  `user_id` INT NOT NULL,
+  `event_id` INT NOT NULL,
+  `creator` TINYINT NULL DEFAULT 0,
+  `active` TINYINT NULL DEFAULT 1,
+  `create_date` DATETIME NULL,
+  PRIMARY KEY (`user_id`, `event_id`),
+  INDEX `fk_user_has_event_event1_idx` (`event_id` ASC),
+  INDEX `fk_user_has_event_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_user_has_event_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_event_event1`
+    FOREIGN KEY (`event_id`)
+    REFERENCES `event` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user_details`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user_details` ;
+
+CREATE TABLE IF NOT EXISTS `user_details` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_detail_name` VARCHAR(45) NULL,
+  `user_detail_description` VARCHAR(500) NULL,
+  `user_detail_url` VARCHAR(20000) NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_user_details_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_user_details_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `event_img`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `event_img` ;
+
+CREATE TABLE IF NOT EXISTS `event_img` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `event_img_url` VARCHAR(20000) NULL,
+  `event_img_name` VARCHAR(100) NULL,
+  `event_id` INT NOT NULL,
+  `create_date` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_event_img_event1_idx` (`event_id` ASC),
+  CONSTRAINT `fk_event_img_event1`
+    FOREIGN KEY (`event_id`)
+    REFERENCES `event` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 SET SQL_MODE = '';
@@ -40,13 +261,3 @@ GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'travelboreduser'@'l
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
--- -----------------------------------------------------
--- Data for table `user`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `travelboreddb`;
-INSERT INTO `user` (`id`, `username`, `password`, `active`, `role`) VALUES (1, 'admin', 'admin', 1, 'ADMIN');
-
-COMMIT;
-
