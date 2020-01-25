dbeaa371c4bc3f672c43f847321bfdadd5cab28d
Erin Eustice <erinrenee.va@gmail.com>
2020-01-25 14:00:06 -0700

    event controller, redirect controller, user controller

--- TravelBored/src/main/java/com/skilldistillery/travelboard/controllers/EventController.java
+++ TravelBored/src/main/java/com/skilldistillery/travelboard/controllers/EventController.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.travelboard.controllers;
 
 public class EventController {
+	
+	
 
 }
--- TravelBored/src/main/java/com/skilldistillery/travelboard/controllers/UserController.java
+++ TravelBored/src/main/java/com/skilldistillery/travelboard/controllers/UserController.java
@@ -2,20 +2,48 @@ package com.skilldistillery.travelboard.controllers;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.skilldistillery.travelboard.data.DAOUser;
+import com.skilldistillery.travelboard.entities.Location;
 import com.skilldistillery.travelboard.entities.User;
 
 @Controller
 public class UserController {
-	
-	@RequestMapping(path="logout.do", method = RequestMethod.GET)
-	public String userLogout() {
+	@Autowired
+	private DAOUser daoUser;
+
+	@RequestMapping(path = "logout.do", method = RequestMethod.GET)
+	public String userLogout(HttpSession session) {
+		session.removeAttribute("loggedInUser");
 		return "landing";
-		
+
 	}
 
+	@RequestMapping(path = "register.do", method = RequestMethod.POST)
+	public String register(HttpSession session, User user) {
+		Location location = new Location();
+		location.setId(1);
+		user.setLocation(location);
+		user = daoUser.create(user);
+		session.setAttribute("loggedInUser", user);
+		return "profile";
+	}
+
+	@RequestMapping(path = "disableProfile.do", method = RequestMethod.GET)
+	public String disable(HttpSession session) {
+
+		User user = (User) session.getAttribute("loggedInUser");
+		user.setActive(false);
+		return "adminsettings";
+
+	}
+	@RequestMapping(path = "updateProfile.do", method = RequestMethod.POST)
+	public String update(User user, int userId) {
+		user = daoUser.updateBasicUserInfo(user, userId);
+		return "profile";
+	}
 }
