1d5857523e47e50dee2ccd6ebca07700b4ce3ab0
Travis Roberts <travis.c.roberts35@gmail.com>
2020-01-31 08:44:45 -0700

    updated DB added admin and removed event to show during presentation

--- DB/travelbored.sql
+++ DB/travelbored.sql
@@ -288,6 +288,7 @@ START TRANSACTION;
 USE `travelboreddb`;
 INSERT INTO `user` (`id`, `username`, `password`, `active`, `role`, `first_name`, `last_name`, `email`, `location_id`, `create_date`, `user_img_url`) VALUES (2, 'LoneTraveler', 'lonetraveler', 1, 'user', 'Jack', 'Ryan', 'jackryan@gmail.com', 2, '2020-01-24', 'https://skilldistillery.com/wp-content/uploads/2017/01/RobRPicSDWEB-250x250.jpg');
 INSERT INTO `user` (`id`, `username`, `password`, `active`, `role`, `first_name`, `last_name`, `email`, `location_id`, `create_date`, `user_img_url`) VALUES (1, 'TravisR', 'admin', 1, 'admin', 'Travis', 'Roberts', 'travis.c.roberts35@gmail.com', 1, '2020-01-30', 'https://scontent.fapa1-1.fna.fbcdn.net/v/t1.0-9/1503400_2558642324082_138437503_n.jpg?_nc_cat=101&_nc_ohc=MnurFJGlohAAX8TVbjx&_nc_ht=scontent.fapa1-1.fna&oh=98565d423eaa629d981abc845567815a&oe=5EC7675F');
+INSERT INTO `user` (`id`, `username`, `password`, `active`, `role`, `first_name`, `last_name`, `email`, `location_id`, `create_date`, `user_img_url`) VALUES (3, 'Admin', 'secretpassword', 1, 'admin', 'Admin', '', 'admin@travelbored.com', 2, '2020-01-31', 'https://cdn0.iconfinder.com/data/icons/man-user-human-profile-avatar-person-business/100/10B-1User-512.png');
 
 COMMIT;
 
@@ -297,24 +298,11 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `travelboreddb`;
-INSERT INTO `event` (`id`, `title`, `hook`, `description`, `event_time`, `event_date`, `img_url`, `event_url`, `active`, `location_id`, `create_date`) VALUES (1, 'Skill Distillery SuperBowl LIV Party', 'Join us for the first Skill Distillery SuperBowl Party hosted right here at Skill Distillery', 'Super Bowl LIV, the 54th Super Bowl and the 50th modern-era National Football League championship game, will decide the champion for the NFLs 2019 and 100th season. The National Football Conference champion San Francisco 49ers will play against the American Football Conference champion Kansas City Chiefs.', '16:30', '2020-02-02', 'https://metrovoicenews.com/wp-content/uploads/2019/02/mahomes-points-up.jpg', 'https://www.nfl.com/super-bowl', 1, 2, '2020-01-30');
 INSERT INTO `event` (`id`, `title`, `hook`, `description`, `event_time`, `event_date`, `img_url`, `event_url`, `active`, `location_id`, `create_date`) VALUES (2, 'Beer & Food Tour of the Rino Arts District', 'On this three-hour food and beer adventure, taste some of the best bites and brews Denver has to offer and explore the RiNo Arts District.', 'We’ll start and end inside the beautiful Denver Central Market, a hub for gourmet vendors and a popular hang-out spot for Denver locals. Along the way, we’ll walk to five restaurants and try delicious local dishes with optional beer pairings. You’ll taste authentic wood-fired pizza, fresh sushi-grade poke with salmon, Mexican tacos from one of Denver’s top chefs, and the best selling ice cream from one of Food and Wine Magazine’s top ice creameries in the country.', '14:00', '2020-01-31', 'https://assets.simpleviewinc.com/simpleview/image/fetch/c_fill,h_465,q_50,w_698/https://assets.simpleviewinc.com/simpleview/image/upload/crm/denver/DeliciousDenver_238148EA-D3FE-4D3D-9149BC895E64007D_62ca1b5f-a934-4198-8da780e781d2eba6.jpg', 'https://www.deliciousdenverfoodtours.com/foodandbeertour/', 1, 4, '2020-01-30');
 INSERT INTO `event` (`id`, `title`, `hook`, `description`, `event_time`, `event_date`, `img_url`, `event_url`, `active`, `location_id`, `create_date`) VALUES (3, 'Beer Here! Brewing the New West', 'A river of beer flows through Colorados past.', ' Come explore our brewing past, present, and future. See beer-brewing equipment from then to now, a massive bottle-breaker from Prohibition days, the nations first aluminum beer cans, and a wealth of other artifacts from our states hoppy history.', '10:00', '2020-01-31', 'https://assets.simpleviewinc.com/simpleview/image/fetch/c_fill,h_465,q_50,w_698/https://assets.simpleviewinc.com/simpleview/image/upload/crm/denver/HC_beerHere_sm-010_4bd3234c-a11f-e1f8-475436eef17c3eff.png', 'https://www.historycolorado.org/exhibit/beer-here', 1, 10, '2020-01-30');
 INSERT INTO `event` (`id`, `title`, `hook`, `description`, `event_time`, `event_date`, `img_url`, `event_url`, `active`, `location_id`, `create_date`) VALUES (4, 'Icelantics Winter on the Rocks', 'AEG Presents is thrilled to announce ICELANTICS WINTER ON THE ROCKS FT. ZHU live at Red Rocks Amphitheatre on Friday, January 31st, 2020.', 'Tickets to this show are available at the Denver Coliseum box office from 11 a.m. – 3 p.m. every Saturday. All tickets sold at the Coliseum box office require a Flash Seats account along with a valid ID matching the account profile.  Download the Red Rocks App before your visit. From purchasing and managing your tickets, to park and venue maps, tips, weather alerts and special offers, the official Red Rocks app has everything you need for an unforgettable concert experience.', '18:45', '2020-01-31', 'https://assets.simpleviewinc.com/simpleview/image/fetch/q_75/https://assets.simpleviewinc.com/simpleview/image/upload/crm/denver/icelantic-s-winter-on-the-rocks-ft-zhu-tickets_01-31-20_18_5dc9f81acb4730_2dda1573-fb8f-a3db-a02d226ce425f498.jpg', 'https://www.redrocksonline.com/events/detail/icelantics-winter-on-the-rocks-ft-zhu', 1, 11, '2020-01-30');
 INSERT INTO `event` (`id`, `title`, `hook`, `description`, `event_time`, `event_date`, `img_url`, `event_url`, `active`, `location_id`, `create_date`) VALUES (5, 'Breckenridge Mardis Gras', 'Each year, New Orleans visitors and Mardi Gras lovers take over Breckenridge with a parade, street party, masks, boas, music, and revelry to commemorate the carnival season. ', 'This year, we are thrilled to be bringing in some of the most seasoned and highly respected musicians from New Orleans to feature a truly authentic New Orleans Mardi Gras Celebration!', '17:30', '2020-02-25', 'https://1ln40d4elc1oqb3b4do821oz-wpengine.netdna-ssl.com/wp-content/uploads/2018/01/dog-in-costume-during-breckenridge-mardi-gras-214x300.jpg', 'https://www.gobreck.com/event/mardi-gras/', 1, 12, '2020-01-30');
 INSERT INTO `event` (`id`, `title`, `hook`, `description`, `event_time`, `event_date`, `img_url`, `event_url`, `active`, `location_id`, `create_date`) VALUES (6, 'Bighorn Sheep Day Festival', 'Kids of all ages can see our state mammal in its natural habitat, learn fascinating animal facts and make crafts.', 'Join us for the 15th Annual Bighorn Sheep Day. Look for bighorns in their natural habitat through our high-power telescopes. Bighorn presentations and door prizes. Guided nature walks. Children‘s activities and crafts. Live Zoo animals. No-charge - all activities are FREE! Don‘t forget your binoculars. It‘s a full day of free family fun!', '10:00', '2020-02-15', 'https://cdn.britannica.com/93/6493-004-9A40A729/Bighorn-sheep.jpg', 'https://www.visitcos.com/events-calendar/annual/bighorn-sheep-day/', 1, 6, '2020-01-30');
-
-COMMIT;
-
-
--- -----------------------------------------------------
--- Data for table `event_detail`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `travelboreddb`;
-INSERT INTO `event_detail` (`id`, `detail_name`, `detail_type`, `active`, `event_id`) VALUES (1, 'Dress Code', 'Black Tie Optional', 1, 1);
-INSERT INTO `event_detail` (`id`, `detail_name`, `detail_type`, `active`, `event_id`) VALUES (2, 'Dress Code', 'Casual', 1, 2);
-INSERT INTO `event_detail` (`id`, `detail_name`, `detail_type`, `active`, `event_id`) VALUES (3, 'Dress Code', 'Casual', 1, 3);
 
 COMMIT;
 
@@ -346,7 +334,6 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `travelboreddb`;
-INSERT INTO `user_event` (`user_id`, `event_id`, `creator`, `active`, `create_date`) VALUES (1, 1, 1, 1, '2020-01-24');
 INSERT INTO `user_event` (`user_id`, `event_id`, `creator`, `active`, `create_date`) VALUES (1, 2, 1, 1, '2020-01-30');
 INSERT INTO `user_event` (`user_id`, `event_id`, `creator`, `active`, `create_date`) VALUES (1, 3, 1, 1, '2020-01-30');
 
